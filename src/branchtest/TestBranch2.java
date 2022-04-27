846f516f7937c0e72875f0b7bfc47aa20449cc9e
youngwoo.bang <youngwoo.bang@kt.com>
2022-04-27 16:05:11 +0900

    branch test 2

--- src/branchtest/TestBranch2.java
+++ src/branchtest/TestBranch2.java
@@ -3,5 +3,7 @@ package branchtest;
 public class TestBranch2 {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
     }
 }
