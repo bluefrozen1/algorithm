d455a5a6afeb806ecf7a99bcb0f163e138b3ef13
youngwoo.bang <bluefrozen2@gmail.com>
2022-05-03 22:12:39 +0900

    br1

--- src/branchtest/TestBranch2.java
+++ src/branchtest/TestBranch2.java
@@ -4,6 +4,6 @@ public class TestBranch2 {
     public static void main(String[] args) {
         System.out.println("args21 = " + args);
         System.out.println("args2 = " + args);
-        System.out.println("3 = " + args);
+        System.out.println("333 = " + args);
     }
 }
